c382a75a9b1a2f90f68a9af94bd6ba6d48c0d6ab
Lefumeux Bastien <bastien.lefumeux@etu.unice.fr>
2025-03-11 16:07:54 +0100

    feat(user) : update user model

--- packages/api/models/User.js
+++ packages/api/models/User.js
@@ -5,8 +5,15 @@ const userSchema = new Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   role: { type: String, required: true, default: "user" },
-  createAt: { type: Date, default: Date.now },
+  createAt: { type: Date, default: Date.now , immutable : true },
   lastConnection: { type: Date, default: Date.now },
+  prefTheme: { type: String, default: "light" },
+  stats : {
+    pixelBoardsCreated: { type: Number, default: 0 },
+    pixelBoardsParticipated: { type: Number, default: 0 },
+    pixelPainted: { type: Number, default: 0 },
+    lastPixelTouched : { type: Date, default: Date.now }
+  },
 });
 
 userSchema.methods.toJSON = function () {
