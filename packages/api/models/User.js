f986c8934a4eb55bf2d15a186f94a0304d600478
Lefumeux Bastien <bastien.lefumeux@etu.unice.fr>
2025-03-16 16:36:19 +0100

    feat(user) : Ajout d'un attribut avatar dans le model

--- packages/api/models/User.js
+++ packages/api/models/User.js
@@ -2,6 +2,7 @@ import mongoose, { Schema } from "mongoose";
 
 const userSchema = new Schema({
   name: { type: String, required: true, unique: true },
+  avatar: { type: String, default: undefined },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   role: { type: String, required: true, default: "user" },
